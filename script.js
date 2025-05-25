0f973a8db359571f3e292a0cd6473c883ec71a5a
Faturahman44 <paturahman.alwapiq11@admin.smp.belajar.id>
2025-05-25 11:07:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,42 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentPage = 0;
 
   const flipSound = new Audio("audio/flip.mp3");
+  // ❄️ Efek Salju Putih ❄️
+  function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.textContent ="✻";
+    snowflake.classList.add("snowflake");
+    snowflake.style.position = "fixed";
+    snowflake.style.top = "-10px";
+    snowflake.style.left = Math.random() * 100 + "vw";
+    snowflake.style.fontSize = Math.random() * 10 + 10 + "px";
+    snowflake.style.opacity = Math.random();
+    snowflake.style.color = "white";
+    snowflake.style.zIndex = 999;
+    snowflake.style.pointerEvents = "none";
+    snowflake.style.animation = `fall ${Math.random() * 5 + 5}s linear`;
+
+    document.body.appendChild(snowflake);
+
+    // Hapus setelah animasi selesai
+    setTimeout(() => {
+      snowflake.remove();
+    }, 10000);
+  }
+
+  setInterval(createSnowflake, 200);
+
+  // Tambahkan animasi CSS lewat JS
+  const style = document.createElement('style');
+  style.innerHTML = `
+    @keyframes fall {
+      to {
+        transform: translateY(100vh) rotate(360deg);
+      }
+    }
+  `;
+  document.head.appendChild(style);
+
 
     // === TAMBAHAN TOMBOL DARK MODE DI POJOK KANAN ATAS ===
   const toggleButton = document.createElement("button");
@@ -34,13 +70,13 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   const loveTexts = [
-    "jadi kekasihku saja",
-    "katakan cinta💖",
-    "hati ini meminta",
-    "kau lebih dari teman berbagi",
-    "jadi kekasih ku saja",
-    "cinta katakan cinta💖",
-    "hati ini meminta",
+    "jadi kekasihku saja.",
+    "katakan cinta💖.",
+    "hati ini meminta.",
+    "kau lebih dari teman berbagi.",
+    "jadi kekasih ku saja.",
+    "cinta katakan cinta💖.",
+    "hati ini meminta .",
     "kau lebih dari teman berbagi 💖"
     
     
@@ -78,6 +114,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Pas load pertama langsung buka halaman pertama dengan animasi teks
   openPage(0);
+
+// === Play musik saat foto pertama diklik ===
+const bgMusic = document.getElementById("bg-music");
+let musicStarted = false;
+
+document.querySelector(".book").addEventListener("click", function () {
+  if (!musicStarted) {
+    bgMusic.play().catch(e => {
+      console.log("Auto-play ditolak, user harus interaksi dulu.");
+    });
+    musicStarted = true;
+  }
+});
+
 
   // Tombol scroll ke video
   scrollBtn.addEventListener("click", function (e) {
